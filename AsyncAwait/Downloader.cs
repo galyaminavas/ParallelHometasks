c8486bf816083928bb2328101e29ceed2625d919
galyaminavas <galyaminavas@gmail.com>
2017-12-14 01:15:15 +0300

    AsyncAwait task

---
+++ AsyncAwait/Downloader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebPagesDownload
+{
+    public class Downloader
+    {
+        public async Task<String> DownloadPage(string address)
+        {
+            using (WebClient client = new WebClient())
+            {
+                String content = "";
+                try
+                {
+                    content = await client.DownloadStringTaskAsync(address);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message + "\nOn page: " + address);
+                }
+                return content;
+            }
+        }
+
+        public String[] FindLinks(string mainPage)
+        {
+            //<a href="http://...">
+            Regex tagRX = new Regex(@"<a href=("")http(s)?://[a-zA-Z0-9-_\.~/:#\?=%&;\+@!\$\*',\[\]\(\)]*("")>");
+            var matches = tagRX.Matches(mainPage);
+            string[] links = new string[matches.Count];
+            string pattern1 = @"<a href=("")";
+            string pattern2 = @"("")>";
+            Regex rgx1 = new Regex(pattern1);
+            Regex rgx2 = new Regex(pattern2);
+            for (int i = 0; i < matches.Count; i++)
+            {
+                links[i] = matches[i].ToString();
+                links[i] = rgx1.Replace(links[i], "");
+                links[i] = rgx2.Replace(links[i], "");
+            }
+            return links;
+        }
+    }
+}
---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebPagesDownload
+{
+    class Program
+    {
+        public static void LinksInfo(string addr)
+        {
+            Console.WriteLine("Web page address: " + addr);
+            Console.WriteLine();
+
+            Downloader dwnldr = new Downloader();
+            var mainPage = dwnldr.DownloadPage(addr);
+            var innerPages = dwnldr.FindLinks(mainPage.Result);
+            Console.WriteLine("Wait for it...\n");
+            foreach (var p in innerPages)
+            {
+                var len = dwnldr.DownloadPage(p).Result.Length;
+                Console.WriteLine("Page: " + p + " -- Size: " + len);
+            }
+            Console.WriteLine();
+            Console.WriteLine("That's all.");
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter web page address");
+            //string address = Console.ReadLine();
+
+            string address = "http://www.natel.ru/links";
+            //string address = "https://www.topuniversities.com/blog/33-useful-websites-students";
+            //string address = "http://google.com";
+            //string address = "https://www.labnol.org/internet/101-useful-websites/18078/";
+
+            LinksInfo(address);
+
+            Console.ReadKey();
+        }
+    }
+}
